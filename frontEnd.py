c6ce269fa70b98554472198e94d9dbea135fdac8
Lauren Field <lf4g23@soton.ac.uk>
2024-11-16 15:30:31 +0000

    SideBar creation

--- frontEnd.py
+++ frontEnd.py
@@ -1,30 +1,10 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+from streamlit_option_menu import option_menu
 
-def home_page():
-    st.title("Home Page")
-    st.write("Welcome to the Home Page!")
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Home","Graph", "Animation","Settings"],
+    icons = ['house', 'graph-up', 'tv','gear'], menu_icon = "cast", default_index=1)
 
-def about_page():
-    st.title("About Page")
-    st.write("This is the About Page!")
-
-def contact_page():
-    st.title("Contact Page")
-    st.write("Reach out to us on the Contact Page!")
-
-# Sidebar navigation
-st.sidebar.title("Navigation")
-menu = st.sidebar.radio(
-    "Go to",
-    options=["Home", "About", "Contact"]
-)
-
-# Render the selected page
-if menu == "Home":
-    home_page()
-elif menu == "About":
-    about_page()
-elif menu == "Contact":
-    contact_page()
+selected 
