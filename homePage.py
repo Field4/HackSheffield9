53715f3e5f5d4563958c53ce894ddb10c34e1c73
Lauren Field <lf4g23@soton.ac.uk>
2024-11-16 17:47:18 +0000

    Home page message

--- homePage.py
+++ homePage.py
@@ -2,4 +2,7 @@ import streamlit as st
 
 def showPage():
     st.title("Home")
+    st.markdown("This project is a simulation of the growth and decline in a population. It represents three elements of an ecosystem: The producers, the herbivores and the carnivores.")
+    st.markdown("We have produced two ways of viewing the results:")
+    st.markdown(" - The first way is using graphs - We have one graph per population so you can see how they grow and decline over time")
 
