693fbf9556edb51a1efd52b56a8f5259a60efe14
Lauren Field <lf4g23@soton.ac.uk>
2024-11-16 18:26:53 +0000

    Home page Update

--- homePage.py
+++ homePage.py
@@ -5,4 +5,5 @@ def showPage():
     st.markdown("This project is a simulation of the growth and decline in a population. It represents three elements of an ecosystem: The producers, the herbivores and the carnivores.")
     st.markdown("We have produced two ways of viewing the results:")
     st.markdown(" - The first way is using graphs - We have one graph per population so you can see how they grow and decline over time")
+    st.markdown(" - The second way is through an animation on a map, (explain map here)")
 
