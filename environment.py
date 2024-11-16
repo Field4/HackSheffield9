3e554e577305a3015a7784804343a772b5b1d800
lottie <lottiesletterbox@protonmail.com>
2024-11-16 14:37:01 +0000

    things are happenign

--- environment.py
+++ environment.py
@@ -1,18 +1,23 @@
+MAX_TEMP = 30
+
 class Environment:
     temperature = 15
     sunlight = 0.5
 
-    plantLifeAvailable = 100
+    plantGrowthFactor = (temperature * sunlight) / MAX_TEMP # (times 1)
 
-    def __init__(self, t : int, s : float):
+    plantLifeAvailable : float = 100
+
+    def __init__(self, t : int, s : float, initialFood : int):
         self.temperature = t
         self.sunlight = s
+        self.plantLifeAvailable = initialFood
+        self.plantGrowthFactor = (self.temperature * self.sunlight) / MAX_TEMP
 
     def generatePlantLife(self):
-        self.plantLifeAvailable =  self.plantLifeAvailable * self.temperature * self.sunlight
+        self.plantLifeAvailable =  self.plantLifeAvailable * self.plantGrowthFactor
 
     def eatPlants(self, amountEaten : int):
         self.plantLifeAvailable -= amountEaten
+        if self.plantLifeAvailable <= 0: self.plantLifeAvailable = 1
     
-newEnv = Environment(15, 0.5)
-print(newEnv.generatePlantLife())
--- main.py
+++ main.py
@@ -1,13 +1,26 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
 from environment import *
 from herbivore import *
 
-env = Environment(15, 0.5)
+env = Environment(15, 0.5, 10000000)
 cow = Herbivore(100, 0.75, 2)
 
-for i in range(10):
+food_available_history = []
+cow_population_history = []
+
+for i in range(50):
     env.generatePlantLife()
     eaten = cow.dieOrReproduce(env.plantLifeAvailable)
-    env.eatPlants(eaten)
+    env.eatPlants(eaten * cow.kill_rate)
     print("Iteration n = " + str(i))
     print("Food available: " + str(env.plantLifeAvailable))
     print("Number of cows: " + str(cow.population))
+    food_available_history.append(env.plantLifeAvailable)
+    cow_population_history.append(cow.population)
+
+#chart_data = pd.DataFrame({"food": food_available_history, "cows": cow_population_history})
+st.line_chart(food_available_history)
+st.line_chart(cow_population_history)
