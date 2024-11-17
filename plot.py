7d6229f5e8a527a46d6e4be25021a975ef8e6f58
Daniel Ray <124963440+Le-Mao@users.noreply.github.com>
2024-11-17 08:33:49 +0000

    plot.py

---
+++ plot.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def output(label: str, value: float, data: dict):
+    print(label, "=", value)
+    if not label in data.keys(): data[label] = []
+    data[label].append(value)
+
+def toFrame(data: dict):
+    return pd.DataFrame(data)
+
+def plotGraph(data: pd.DataFrame):
+    return 0
