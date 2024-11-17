1241f93bf59db77a2ca11afb6f751dbb6c159d16
Daniel Ray <124963440+Le-Mao@users.noreply.github.com>
2024-11-17 11:53:39 +0000

    added initialise function to reseet stuff

--- main.py
+++ main.py
@@ -2,27 +2,35 @@ from environment import *
 from animal import *
 from producer import *
 from plot import *
-import streamlit as sl
-env = Environment(1.2, 300)
-plant = Producer("plant", env, 100)
-moose = Animal("moose", 100, 1, 0.7, 2)
-wolves = Animal("wolf", 100, 0.3, 0.7, 1.5)
-humans = Animal("human", 10, 0.1, 0.1, 1.3)
-moose.food_sources = [plant]
-wolves.food_sources = [moose]
-humans.food_sources = [plant, moose, wolves]
-animals = [moose, wolves, humans]
-plants = [plant]
+#import streamlit as sl
 
-data = {}
+def initialise():
+    env = Environment(1.2, 300)
+    plant = Producer("plant", env, 100)
+    moose = Animal("moose", 100, 1, 0.7, 2)
+    wolves = Animal("wolf", 100, 0.3, 0.3, 1.5)
+    humans = Animal("human", 10, 0.25, 0.1, 1.3)
+    moose.food_sources = [plant]
+    wolves.food_sources = [moose]
+    humans.food_sources = [plant, moose, wolves]
+    animals = [moose, wolves, humans]
+    plants = [plant]
+    return ({}, env, plants, animals)
+
+
+
+
+
 
 NUM_ITERATIONS = 30
-DEATH_RATE = 1.5
+DEATH_RATE = 0.9
 
 def main():
     for i in range(NUM_ITERATIONS):
-        print("Iteration t =", i) # only print time to console, x axis reps time in the graphs
-        output("Plant population", plant.population, data)
+        # only print time to console, x axis reps time in the graphs
+        print("Iteration t =", i) 
+        for plant in plants:
+            output("Plant population", plant.population, data)
         for animal in animals:
             output(animal.name + " population", animal.population, data)
 
@@ -31,12 +39,14 @@ def main():
         
         for animal in animals:
             demand = animal.getDemand()
-            available = animal.food_sources[0].population * animal.feedstock_utilisation
+            total_food = 0
+            for food in animal.food_sources:
+                total_food += food.population
+            available = total_food * animal.feedstock_utilisation
 
             if available > demand:
                 excess = available - demand
                 # cap demand to 125% of required
-                #excessRatio = min(animal.growthRate, excess/demand)
                 # grow based on how much excess food there is
                 animal.grow(animal.population*(animal.growthRate-1))
                 # eat based on new damand
@@ -50,4 +60,5 @@ def main():
 
     return toFrame(data)
 
+(data, env, plants, animals) = initialise()
 main()
