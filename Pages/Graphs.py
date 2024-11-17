0eddeac446e379ee837208509c4a7c4c7737e903
Lauren Field <lf4g23@soton.ac.uk>
2024-11-17 09:42:35 +0000

    Editing for actual backend

--- Pages/Graphs.py
+++ Pages/Graphs.py
@@ -11,14 +11,15 @@ st.set_page_config(
 st.title("Graphs")
 st.markdown("These are our graphs for displaying animal populations and food avilability.")
 col1, col2 = st.columns(2)
-df1, df2, df3 = main.main()
-with col1:
-    st.title("Some Graphs")
-    st.line_chart(df1)
-    st.line_chart(df2)
+# df1, df2, df3 = connectionTest.create_graphs()
+df1 = main.main()
+#with col1:
+st.title("Some Graphs")
+st.line_chart(df1)
+    #st.line_chart(df2)
 
-with col2: 
-    st.title("Other Graphs")
-    st.line_chart(df3)
+#with col2: 
+    #st.title("Other Graphs")
+    #st.line_chart(df3)
 
 
